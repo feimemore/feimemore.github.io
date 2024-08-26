cba04c7b90786f62ea0620610b0c781ef1c054db
feimemore <feimemore@outlook.com>
2024-08-26 15:50:53 +0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -152,4 +152,31 @@ const populateAllPosts = (posts) => {
   });
 };
 
-
+document.getElementById("black").innerHTML = `
+<div class="cat">
+	<div class="ear ear--left"></div>
+	<div class="ear ear--right"></div>
+	<div class="face">
+		<div class="eye eye--left">
+			<div class="eye-pupil"></div>
+		</div>
+		<div class="eye eye--right">
+			<div class="eye-pupil"></div>
+		</div>
+		<div class="muzzle"></div>
+	</div>
+</div>
+<div class="cat">
+	<div class="ear ear--left"></div>
+	<div class="ear ear--right"></div>
+	<div class="face">
+		<div class="eye eye--left">
+			<div class="eye-pupil"></div>
+		</div>
+		<div class="eye eye--right">
+			<div class="eye-pupil"></div>
+		</div>
+		<div class="muzzle"></div>
+	</div>
+</div>
+`
